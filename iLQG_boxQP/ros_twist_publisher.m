db160dc6bac7b4dba21baea7aaf25ab890abfa5d
Aum Jadhav <aum.jadhav@gmail.com>
2017-02-09 23:17:12 -0500

    Publish frequency approx same as traj data

--- iLQG_boxQP/ros_twist_publisher.m
+++ iLQG_boxQP/ros_twist_publisher.m
@@ -1,24 +1,33 @@
+%% Load trajectory & Initialize Global ROS node
+
 load('traj.mat')
 
 rosinit('http://192.168.1.118:11311')
 twist_chatpub = rospublisher('/cmd_vel','geometry_msgs/Twist');
 msg = rosmessage(twist_chatpub);
-pause(2) % Wai to ensure publisher is registered
-%%
+pause(2) % Wait to ensure publisher is registered
 
-wheelbase = 0.257;
-vx = u(1,:);
-steer_angle = acos(vx./sqrt(u(1,:).^2 + u(2,:).^2)); % TODO:Kinematic eqns!
+%% TODO:Kinematic eqns?
+ 
+% wheelbase = 0.257;
+% vx = u(1,:);
+% steer_angle = acos(vx./sqrt(u(1,:).^2 + u(2,:).^2)); 
 
+%% Publish Twist messages
+% Frequency same as traj data
 
-%%
-
-for i=1:size(u,2) 
-    msg.Linear.X = vx(i);
-    msg.Angular.Z = steer_angle(i);
-    send(twist_chatpub,msg);
-    pause(dt)
+% tic
+for i=1:size(u,2)
+    finalTime = datenum(clock + [0, 0, 0, 0, 0, dt]);
+    while datenum(clock) < finalTime
+        msg.Linear.X = u(1,i);
+        msg.Angular.Z = u(2,i);
+        send(twist_chatpub,msg);
+    end
+%     pause(dt-toc) 
 end
+% toc
 
-%%
+%% Shut down global node
+
 rosshutdown
