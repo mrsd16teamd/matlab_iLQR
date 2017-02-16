4959b98e8c42f92e3fae0bb9f35a2a72470ceeac
Kazu Otani <kazuotani14@gmail.com>
2017-02-16 18:09:52 -0500

    sys_id works. test by changing ALG parameter and running on data

--- iLQG_boxQP/sys_id/sys_id.m
+++ iLQG_boxQP/sys_id/sys_id.m
@@ -1,23 +1,22 @@
-function [params] = sys_id(file_path)
+function sys_id()
 
-global msgs_state msgs_cmd;
-global params_to_optimize;
+global stateData;
+global params_to_optimize default_params;
 
-% Log file with control inputs and states with timestamps 
-file_name = 'sudden_obstacle.bag';
-bagfile = rosbag(file_name);
+load('stateData_02-16-17_16-44.mat');
+disp('Loaded data.');
 
-bag_state = select(bagfile, 'Topic', '/scanmatch_odom');
-bag_cmd = select(bagfile, 'Topic', '/cmd_vel');
+%%%%%
+% params = [C_alpha, C_x, Iz, mu, mu_spin]
+params_to_optimize = [1, 1, 1, 1, 1];
+ALG = 1; % 1 = CMA-ES, 2 = fmincon, 3 = fminsearch, 4, = fminunc
+max_f_evals = 100;
 
-msgs_state = readMessages(bag_state);
-msgs_cmd = readMessages(bag_cmd); % Need this to be geometry_msgs/TwistStamped
-
+% Best methods so far: 3
+%%%%
 
 % x = [x, y, phi, Ux, Uy, r]
 % u = [Ux_cmd, delta]
-
-% Choose parameters of interest
 
 m = 2.35;           % mass (kg)
 L = 0.257;          % wheelbase (m)
@@ -25,28 +24,41 @@ g = 9.81;
 b = 0.14328;        % CoG to rear axle
 a = L-b;            % CoG to front axle
 G_rear = m*g*a/L;
-mu = 5.2/G_rear;   
-mu_spin = 4.3/G_rear; 
+mu = 5.2/G_rear;   % currently 0.5098
+mu_spin = 4.3/G_rear; % currently 0.4215
 
-% params = [C_alpha, C_x, Iz, mu, mu_spin]
-params_to_optimize = [1, 1, 1, 1, 1];
-p0 = [300, 330, 0.02065948883, mu, mu_spin];     
+p0 = [300, 330, 0.02065948883, mu, mu_spin];   
+default_params = p0;
+
+% params: [C_alpha, C_x, Iz, mu, mu_spin]
+lb = [100,  100,  0,   0.25, 0.25];
+ub = [1000, 1000, 0.1, 1,    1];
 
 sys_id_criterion(p0);
 
-% Send data to optimizer
-ALG = 1;
-
+disp('Starting optimization...');
 switch ALG
     case 1 % CMA-ES, capped at 500 iterations
-        sigma0 = ones(n_joints*3,1)*0.2; % TODO edit this to adjust to param size
+        sigma0 = ones(length(p0),1)*50; 
         opts = cmaes;
-        opts.MaxIter = 500;
+        opts.MaxIter = max_f_evals;
         [xmin, fmin, counteval, stopflag, out, bestever] = cmaes('sys_id_criterion', p0, sigma0, opts);
         answer = xmin; fval = fmin; exitflag=1;
-    case 2  % SQP
-        options = optimset('Display','iter','MaxFunEvals',1000000,'Algorithm','sqp');
-        [answer,fval,exitflag]=fmincon(@sys_id_criterion,p0, [], [], [], [], [], [], [], options);
+    case 2  % fmincon  
+        % options are 'interior-point', 'trust-region-reflective', 'sqp',
+        % 'active-set'
+        options = optimset('Display','iter','MaxFunEvals',max_f_evals,'Algorithm','interior-point');
+        [answer,fval,exitflag]=fmincon(@sys_id_criterion,p0, [], [], [], [], lb, ub, [], options);
+    case 3
+        options = optimset('Display','iter','MaxFunEvals',max_f_evals,'PlotFcns',@optimplotfval);
+        [answer,fval,exitflag,output] = fminsearch(@sys_id_criterion, p0, options);
+    case 4
+        options = optimset('Display','iter','MaxFunEvals',max_f_evals,'PlotFcns',@optimplotfval);
+        [answer,fval,exitflag,output] = fminunc(@sys_id_criterion, p0, options);     
 end
+
+best_params = answer;
+disp(best_params);
+disp(fval);
 
 end
--- iLQG_boxQP/sys_id/sys_id_criterion.m
+++ iLQG_boxQP/sys_id/sys_id_criterion.m
@@ -3,73 +3,34 @@
 
 function [cost] = sys_id_criterion(params)
 
+    global stateData;
+    global params_to_optimize default_params;
 
-    global msgs_state msgs_cmd;
-    global params_to_optimize;
-
-    params = params.*params_to_optimize;
+    params = params.*params_to_optimize + default_params.*(1-params_to_optimize);
     
     cost = 0;
-    num_states = size(msgs_state, 1);
-
-    current_cmd_index = 1;
+    num_states = size(stateData, 1);
     
     for i = 2:num_states
-        % Read current state
-        [x_observed, t_observed] = getStateAndTimeAt(i);
-        [x_past, t_past] = getStateAndTimeAt(i-1);
+        past = stateData(i-1);
+        now  = stateData(i); 
         
-        % Find control input closest state time in past
-        if(t_cmd > t_past)
-            continue
-        end
-        while(t_cmd < t_past)
-            t_cmd = double(msgs_cmd{current_cmd_index,1}.Header.Stamp.Nsec)/10e9;
-            current_cmd_index = current_cmd_index + 1;
-        end
-        current_cmd_index = current_cmd_index - 1;
-        if ((t_past-t_cmd) > 1.0)
-            % If it's been a long time since control input, disregard
-            continue
-        end
+        dt = (now.Header.Stamp.Nsec - past.Header.Stamp.Nsec)*10^(-9);
         
-        % Assume constant control input between past state and current.
-        % Not necessarily true; input may have changed in between
-        % Check for this? 
-        dt = t_observed - t_past;
-        u = [msgs_cmd{current_cmd_index,1}.Linear.X, ...
-             msgs_cmd{current_cmd_index,1}.Angular.Z];
-     
         % Compare observed state to expected state
         % Make sure to check expected state as function of past input.
-        x_expected = dynamics_finite(x_past, u, dt, params);
+        x_expected = dynamics_finite(past.X', past.U', dt, params);
         
-        cost = cost + metric(x_expected, x_observed);
+        cost = cost + metric(x_expected, now.X');
     end
 end
 
 function [dist] = metric(x1, x2)
-    dist = pdist2(x1, x2, 'euclidean');
-end
-
-function [state, t] = getStateAndTimeAt(index)
-        t = double(msgs_state{index,1}.Header.Stamp.Nsec)/10e9;
-        
-        pos = [msgs_state{index,1}.Pose.Pose.Position.X, ...
-               msgs_state{index,1}.Pose.Pose.Position.Y];
-        
-        vel = [msgs_state{index,1}.Twist.Twist.Linear.X, ...
-               msgs_state{index,1}.Twist.Twist.Linear.Y];
-        
-        q = [msgs_state{index,1}.Pose.Pose.Orientation.W, ...
-             msgs_state{index,1}.Pose.Pose.Orientation.X, ...
-             msgs_state{index,1}.Pose.Pose.Orientation.Y, ...
-             msgs_state{index,1}.Pose.Pose.Orientation.Z];
-        eul = quat2eul(q);
-        theta = eul(1); % default order is zyx
-        
-        angvel = msgs_state{index,1}.Twist.Twist.Angular.Z;
-        
-        state = [pos, theta, vel, angvel];
-
+    % Euclidean distance
+    diff = x1-x2;
+    
+    weights = [5, 5, 5, 1, 1, 1]';  
+    diff = diff.*weights;
+    
+    dist = sqrt(diff'* diff);
 end
