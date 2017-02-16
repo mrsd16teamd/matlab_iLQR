bab8a099f2c827ad4edf056a3bbf0c1e347e55ad
Aum Jadhav <aum.jadhav@gmail.com>
2017-02-15 19:28:17 -0500

    Back to pub Twist msgs

--- iLQG_boxQP/ros_traj_publisher.m
+++ iLQG_boxQP/ros_traj_publisher.m
@@ -1,64 +1,90 @@
-%% Initialize Global ROS node
+%% ROS Trajectory Publisher
 
-rosinit('http://192.168.1.118:11311')
+function ros_traj_publisher(traj_name)
+
+  if nargin < 1
+    traj_name = fullfile(which('traj_smooth.mat'));
+  end
+  load(traj_name)
+
+%% Initialize Global ROS node if not already active
+
+tx1 = rosdevice('192.168.1.118','ubuntu','ubuntu');
+
+try
+    rosinit('http://192.168.1.118:11311')
+catch EXP
+    if string(EXP.identifier)=='robotics:ros:node:GlobalNodeNotRunning'
+        disp('ROS Master not running!')
+    elseif string(EXP.identifier)=='robotics:ros:node:NoMasterConnection'
+        disp('Cannot connect to ROS master! Check network')
+        return
+    end
+end
 
 %% Initialize Publishers
 
 twist_chatpub = rospublisher('/cmd_vel','geometry_msgs/Twist');
+twist_chatpub2 = rospublisher('/cmd_vel_stamped','geometry_msgs/TwistStamped');
+
 twist_msg = rosmessage(twist_chatpub);
-vesc_chatpub = rospublisher('/commands/motor/speed','std_msgs/Float64');
-vesc_msg = rosmessage(vesc_chatpub);
-vel = 1;
+twist_msg2 = rosmessage(twist_chatpub2);
+
+% vesc_chatpub = rospublisher('/commands/motor/speed','std_msgs/Float64');
+% vesc_msg = rosmessage(vesc_chatpub);
 disp('Created following pubishers:')
 disp(twist_chatpub)
-disp(vesc_chatpub)
+% disp(vesc_chatpub)
 
 %% Execute iLQG generated trajectory
 %  Frequency same as traj data
 
-load('traj.mat')
+data_freq = 1/dt;
+target_freq = 100;
+scale = target_freq/data_freq;
 
 % Interpolat data for double frequency
-th=interp1(dt:dt:size(u,2)*dt,u(1,:),dt/2:dt/2:size(u,2)*dt,'spline');
-st=interp1(dt:dt:size(u,2)*dt,u(2,:),dt/2:dt/2:size(u,2)*dt,'spline');
+th=interp1(dt:dt:size(u,2)*dt,u(1,:),dt/scale:dt/scale:size(u,2)*dt,'spline');
+st=interp1(dt:dt:size(u,2)*dt,u(2,:),dt/scale:dt/scale:size(u,2)*dt,'spline');
 u=[th;st];
-
-%% Publish vesc messages
-
-for i=1:size(u,2)
-    finalTime = datenum(clock + [0, 0, 0, 0, 0, dt]);
-    twist_msg.Angular.Z = u(2,i);   % Steering
-    vesc_msg.Data = -1664*u(1,i);   % Throttle
-    while datenum(clock) < finalTime
-        send(twist_chatpub,twist_msg);
-        send(vesc_chatpub,vesc_msg);
-    end
-end
-
-
-% % Debug script
-% while(1)
-%     vesc_msg.Data = -1664*1;   % Throttle
-%     twist_msg.Angular.Z = 0.4;   % Steering
-%     send(twist_chatpub,twist_msg);
-%     send(vesc_chatpub,vesc_msg);
-%     pause(0.05);
-% end
 
 %% Publish Twist messages
 
 % pause(5)
 
-for i=1:size(u,2)
-%     finalTime = datenum(clock + [0, 0, 0, 0, 0, dt]);
-    twist_msg.Linear.X = u(1,i);
-    twist_msg.Angular.Z = 2.75*u(2,i);
-%     while datenum(clock) < finalTime
-        send(twist_chatpub,twist_msg);
-%     end
+for i=1:size(u,2)    
+    twist_msg.Twist.Linear.X = u(1,i);
+    twist_msg.Twist.Angular.Z = u(2,i);
+    send(twist_chatpub2,twist_msg);
     pause(dt)
 end
 
+%% Publish vesc messages
+
+% for i=1:size(u,2)
+%     finalTime = datenum(clock + [0, 0, 0, 0, 0, dt/scale]);
+%     twist_msg.Angular.Z = 1*u(2,i);   % Steering
+%     twist_msg.Linear.X = u(1,i);
+% %     vesc_msg.Data = -1664*u(1,i);   % Throttle
+%     send(twist_chatpub,twist_msg);
+% %     send(vesc_chatpub,vesc_msg);
+%     while datenum(clock) < finalTime
+%     end
+% %     pause(dt)
+% end
+% twist_msg.Angular.Z = 0;   % Steering
+% twist_msg.Linear.X = 0;
+% send(twist_chatpub,twist_msg);
+% disp('done')
+
+% % Debug script
+% while(1)
+%     vesc_msg.Data = -1664*1;   % Throttle
+%     twist_msg.Angular.Z = 0;   % Steering
+%     send(twist_chatpub,twist_msg);
+%     send(vesc_chatpub,vesc_msg);
+%     pause(0.05);
+% end
 
 %% Shut down global node
 
