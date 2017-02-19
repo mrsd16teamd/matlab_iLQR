34829b760e6454dc2d513f95cdf94c821578f9c6
Cyrus Liu <Cyrus@Cyrus-MBP.fios-router.home>
2017-02-19 01:39:35 -0500

    added plot for frechet distance

--- iLQG_boxQP/sys_id/sys_id_plot.m
+++ iLQG_boxQP/sys_id/sys_id_plot.m
@@ -1,4 +1,4 @@
-function sysID_plot% plot state estimate
+function [x,sim_x] = sys_id_plot% plot state estimate
 load('sys_id/experiments/steer_ramp_1L.mat');
 x = zeros(6,size(stateData,1));
 for i = 1:size(stateData,1)
@@ -10,11 +10,32 @@ u = zeros(2,size(stateData,1));
 figure(1);
 axis([-1,2,-1,2]);
 car_plot(x,u);
-inc_steer_sim();
+sim_x = inc_steer_sim();
+
+P = sim_x(1:2,:).';
+Q = x(1:2,:).';
+[cm, cSq] = DiscreteFrechetDist(P,Q);
+% plot result
+figure
+plot(Q(:,1),Q(:,2),'o-r','linewidth',3,'markerfacecolor','r')
+hold on
+plot(P(:,1),P(:,2),'o-b','linewidth',3,'markerfacecolor','b')
+title(['Discrete Frechet Distance of curves P and Q: ' num2str(cm)])
+legend('dt=0.01','dt=0.05','location','best')
+line([2 cm+2],[0.5 0.5],'color','m','linewidth',2)
+text(2,0.4,'dFD length')
+for i=1:length(cSq)
+  line([P(cSq(i,1),1) Q(cSq(i,2),1)],...
+       [P(cSq(i,1),2) Q(cSq(i,2),2)],...
+       'color',[0 0 0]+(i/length(cSq)/1.35));
+end
+axis equal
+% display the coupling sequence along with each distance between points
+disp([cSq sqrt(sum((P(cSq(:,1),:) - Q(cSq(:,2),:)).^2,2))])
 end
 
 % input ramp at line 96
-function inc_steer_sim
+function sim_x = inc_steer_sim()
 show_traj_cog = 1;
 show_traj_r = 1;
 show_wheels = 1;
@@ -49,6 +70,7 @@ axis equal 'auto xy'
 
 % --------Initialize Joystick--------
 x = [0;0;0;0;0;0];
+sim_x = x;
 dt = 0.025;
 T = 5.5;
 throttle = 0;
@@ -96,12 +118,13 @@ for t = 0:dt:T
     
     % constant throttle and steering ramp
     throttle = 1;
-    steer = min(-0.1+t*(1.4/5),0.9); 
+    steer = min(-0.1+t*(1.45/5),0.85); 
     
     % ------Calculate Car Dynamics------
     u = [throttle; steer];
     new_x = dynamics_finite(x, u, dt);
     x = new_x;
+    sim_x = [sim_x x];
     
     traj(i,:) = double(x(1:2));
     i = i+1;
