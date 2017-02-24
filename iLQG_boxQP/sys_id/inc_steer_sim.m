2e34a7007af722c1fa297f91ee5c37736274948e
Cyrus Liu <Cyrus@Cyrus-MBP.wv.cc.cmu.edu>
2017-02-24 15:32:22 -0500

    better plot for sys_id

--- iLQG_boxQP/sys_id/inc_steer_sim.m
+++ iLQG_boxQP/sys_id/inc_steer_sim.m
@@ -1,17 +1,17 @@
 % l_or_r: 1 = left, -1 = right
 % input ramp at line 96
-function [sim_x,sim_u] = inc_steer_sim(thr,l_or_r,rad_s,param)
+function [sim_x,sim_u] = inc_steer_sim(thr,l_or_r,T,param)
 % --------Initialize Joystick--------
 x = [0;0;0;0;0;0];
 sim_x = x;
 sim_u = [0;0];
-dt = 0.025;
-T = 5;
+dt = 0.05;
+rad_s = (7/9 / T)*1.5;
 
 for t = 0:dt:T      
     % constant throttle and steering ramp
     throttle = thr;
-    steer = min(l_or_r*t*rad_s,0.80); 
+    steer = l_or_r*min(t*rad_s,7/9); 
     
     % ------Calculate Car Dynamics------
     u = [throttle; steer];
--- iLQG_boxQP/sys_id/sys_id_plot.m
+++ iLQG_boxQP/sys_id/sys_id_plot.m
@@ -1,57 +1,87 @@
-function sys_id_plot% plot state estimate
-load('sys_id/experiments/steer_ramp_2L.mat');
-x = zeros(6,size(stateData,1));
-for i = 1:size(stateData,1)
-    x(:,i) = stateData(i).X';
+function sys_id_plot(param,thr)% plot state estimate
+% sys_id_plot(param) to show all ramp trajectories at different speed
+% sys_id_plot(param, thr) to show ramp trajectory at thr m/s
+if ~exist('thr','var')
+    subplot(1,3,1);
+    axis square
+    sys_id_plot(param,1);
+    
+    subplot(1,3,2);
+    axis square
+    sys_id_plot(param,2);
+    
+    subplot(1,3,3);
+    axis square
+    sys_id_plot(param,3);
+    
+    return
+elseif thr == 1
+    load('sys_id/experiments/round2/ramp_1left.mat');
+    xL = zeros(6,size(stateData,1));
+    for i = 1:size(stateData,1)
+        xL(:,i) = stateData(i).X';
+    end
+    
+    load('sys_id/experiments/round2/ramp_1right.mat');
+    xR = zeros(6,size(stateData,1));
+    for i = 1:size(stateData,1)
+        xR(:,i) = stateData(i).X';
+    end
+    
+    [sim_x, sim_u] = inc_steer_sim(1,1,5,param);
+elseif thr == 2
+    load('sys_id/experiments/round2/ramp_2left_v3.mat');
+    xL = zeros(6,size(stateData,1));
+    for i = 1:size(stateData,1)
+        xL(:,i) = stateData(i).X';
+    end
+    
+    load('sys_id/experiments/round2/ramp_2right.mat');
+    xR = zeros(6,size(stateData,1));
+    for i = 1:size(stateData,1)
+        xR(:,i) = stateData(i).X';
+    end
+    
+    [sim_x, sim_u] = inc_steer_sim(2,1,2,param);
+elseif thr == 3
+    load('sys_id/experiments/round2/ramp_3left.mat');
+    xL = zeros(6,size(stateData,1));
+    for i = 1:size(stateData,1)
+        xL(:,i) = stateData(i).X';
+    end
+    
+    load('sys_id/experiments/round2/ramp_3right_v2.mat');
+    xR = zeros(6,size(stateData,1));
+    for i = 1:size(stateData,1)
+        xR(:,i) = stateData(i).X';
+    end
+    
+    [sim_x, sim_u] = inc_steer_sim(3,1,2,param);
 end
+
+
 % normalize to origin
-x = bsxfun(@minus,x,[x(1,1);x(2,1);x(3,1);0;0;0]);
+theta = -xL(3,1);
+xL = bsxfun(@minus,xL,[xL(1,1);xL(2,1);xL(3,1);0;0;0]);
+R = [cos(theta) -sin(theta); sin(theta) cos(theta)];
+xL(1:2,:) = R*xL(1:2,:);
 u = zeros(2,size(stateData,1));
 figure(1);
-subplot(1,3,1);
 axis([-1,2,-1,2]);
-car_plot(x,u);
-param = [28,19,0.021,0.57,0.20];
-% param = [23.7;17.3;0.021;0.66;0.20];
-[sim_x, sim_u] = inc_steer_sim(2,1,0.20,param);
-car_plot(sim_x,sim_u,'b');
-P = sim_x(1:2,:).';
-Q = x(1:2,:).';
-cm = DiscreteFrechetDist(P,Q);
+car_plot(xL,u);
 
 
-load('sys_id/experiments/steer_ramp_3L.mat');
-x = zeros(6,size(stateData,1));
-for i = 1:size(stateData,1)
-    x(:,i) = stateData(i).X';
-end
 % normalize to origin
-x = bsxfun(@minus,x,[x(1,1);x(2,1);x(3,1);0;0;0]);
+theta = -xR(3,1);
+xR = bsxfun(@minus,xR,[xR(1,1);xR(2,1);xR(3,1);0;0;0]);
+R = [cos(theta) -sin(theta); sin(theta) cos(theta)];
+xR(1:2,:) = R*xR(1:2,:);
+xR(2:3,:) = -xR(2:3,:);
 u = zeros(2,size(stateData,1));
-subplot(1,3,2);
+figure(1);
 axis([-1,2,-1,2]);
-car_plot(x,u);
-[sim_x, sim_u] = inc_steer_sim(3,1,0.16,param);
-car_plot(sim_x,sim_u,'b');
-P = sim_x(1:2,:).';
-Q = x(1:2,:).';
-cm = cm+DiscreteFrechetDist(P,Q);
+car_plot(xR,u);
 
-load('sys_id/experiments/steer_ramp_4L.mat');
-x = zeros(6,size(stateData,1));
-for i = 1:size(stateData,1)
-    x(:,i) = stateData(i).X';
-end
-% normalize to origin
-x = bsxfun(@minus,x,[x(1,1);x(2,1);x(3,1);0;0;0]);
-u = zeros(2,size(stateData,1));
-subplot(1,3,3);
-axis([-1,2,-1,2]);
-car_plot(x,u);
-[sim_x, sim_u] = inc_steer_sim(4,1,0.16,param);
 car_plot(sim_x,sim_u,'b');
-P = sim_x(1:2,:).';
-Q = x(1:2,:).';
-cm = cm+DiscreteFrechetDist(P,Q)
 end
 
