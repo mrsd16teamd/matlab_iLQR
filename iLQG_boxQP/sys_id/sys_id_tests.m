e661d6efde76057cf1111fde32093effa7cbb6cd
Aum Jadhav <aum.jadhav@gmail.com>
2017-02-27 19:28:53 -0500

    Updated sys_id_tests

--- iLQG_boxQP/sys_id/sys_id_tests.m
+++ iLQG_boxQP/sys_id/sys_id_tests.m
@@ -6,31 +6,33 @@
 
 rosinit('http://192.168.1.118:11311')
 
-%% Initialize Publishers
+% Initialize Publishers
 
 twist_chatpub = rospublisher('/cmd_vel','geometry_msgs/Twist');
 twist_msg = rosmessage(twist_chatpub);
-vesc_chatpub = rospublisher('/commands/motor/speed','std_msgs/Float64');
-vesc_msg = rosmessage(vesc_chatpub);
-vel = 1;
+% vesc_chatpub = rospublisher('/commands/motor/speed','std_msgs/Float64');
+% vesc_msg = rosmessage(vesc_chatpub);
 disp('Created following pubishers:')
 disp(twist_chatpub)
-disp(vesc_chatpub)
+% disp(vesc_chatpub)
 
 %%  Tire Stiffness Experiment (Steering Ramp)
  
 dt = 0.05; % Publish period
-time_horizon = 5;
-steer_angles = linspace(0,0.96,time_horizon/dt);
-lin_vel = 0.5;
-vesc_msg.Data = -1664*lin_vel;  % Vx
- 
+time_horizon = 2;
+steer_angles = -linspace(0,0.7777,time_horizon/dt);
+lin_vel = 3;
+
 for i=1:length(steer_angles)
     twist_msg.Angular.Z = steer_angles(i);
+    twist_msg.Linear.X = lin_vel;
     send(twist_chatpub,twist_msg);
-    send(vesc_chatpub,vesc_msg);
     pause(dt)
 end
+
+twist_msg.Linear.X = 0;
+twist_msg.Angular.Z = 0;
+send(twist_chatpub,twist_msg);
 
 ctrl_ip = [repmat(lin_vel,1,length(steer_angles));steer_angles];
 
@@ -55,17 +57,25 @@ end
 % file_name = ['launch',datestr(now,'_mm-dd-yy_HH:MM')];
 % save([file_name,'.mat'],'time_horizon','lin_vel');
 
+rosshutdown
+
 %% Donuts/Figure of 8 drifting
  
-max_steer = 0.96;
-max_vel = 4;
-time_horizon = 5;
+max_steer = 0.77;
+fraction = 2/3;
+max_vel = 0.25;
+time_horizon = 15;
+
+twist_msg.Linear.X = 0;
+twist_msg.Angular.Z = fraction*max_steer;
+send(twist_chatpub,twist_msg);
+pause(2)
+
 finalTime = datenum(clock + [0, 0, 0, 0, 0, time_horizon]);
 while datenum(clock) < finalTime
-    twist_msg.Angular.Z = max_steer;
-    vesc_msg.Data = -1664*max_vel;
+    twist_msg.Angular.Z = fraction*max_steer;
+    twist_msg.Linear.X = max_vel;
     send(twist_chatpub,twist_msg);
-    send(vesc_chatpub,vesc_msg);
 end
 % % Uncomment loop below for figure of 8 drifting
 % finalTime = datenum(clock + [0, 0, 0, 0, 0, time_horizon]);
@@ -76,3 +86,8 @@ end
 %     send(vesc_chatpub,vesc_msg);
 % end
 
+twist_msg.Linear.X = 0;
+twist_msg.Angular.Z = 0;
+send(twist_chatpub,twist_msg);
+
+% rosshutdown
